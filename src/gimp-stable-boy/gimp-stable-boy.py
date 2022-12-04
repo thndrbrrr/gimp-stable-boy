3d7d911b0b9a044ec50b36c530a52765fa53c8c6
danielalcalde <daniel.alcaldepuente@rub.de>
2022-12-04 17:40:24 +0100

    fixed

--- src/gimp-stable-boy/gimp-stable-boy.py
+++ src/gimp-stable-boy/gimp-stable-boy.py
@@ -175,8 +175,6 @@ def run(*args, **kwargs):
 def run_txt2img(*args, **kwargs):
     kwargs.update(dict(zip((param[1] for param in GIMP_PARAMS['TXT2IMG']), args)))
     kwargs['mode'] = 'TXT2IMG'
-    print(kwargs)
-    f.flush()
     run(args, **kwargs)
 
 
